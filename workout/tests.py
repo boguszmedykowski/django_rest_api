4c0d03591b814ec130d9997afd94bb05f3c7592f
boguszmedykowski <bogusz.medykowski@gmail.com>
2023-03-04 16:59:14 +0100

    add succesfull Tests of Views

--- workout/tests.py
+++ workout/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from .models import Exercise, Workout
 
-# Create your tests here.
+
+class TestViews(APITestCase):
+    url = "/workouts/workouts/1/"
+    data = {'url': 'http://testserver/workouts/workouts/1/', 'title': 'treningA'}
+
+    def setUp(self):
+        Workout.objects.create(title="treningA")
+
+    def test_get_exercise(self):
+        response = self.client.get(self.url)
+        result = response.json()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(result, self.data)
