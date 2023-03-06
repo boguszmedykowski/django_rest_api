a6e8cc7ac6888991b4fe5fdb5e6966728fb376cb
boguszmedykowski <bogusz.medykowski@gmail.com>
2023-03-06 17:32:26 +0100

    install coverage

--- workout/admin.py
+++ workout/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from .models import Exercise, Workout
 
 
+# @admin.register(Exercise)
+# class ExerciseAdmin(admin.ModelAdmin):
+#     list_display = ('title', 'id', 'author')
+
+
 admin.site.register(Exercise)
 admin.site.register(Workout)
 
