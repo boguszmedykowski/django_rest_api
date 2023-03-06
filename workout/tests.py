2da147c9fc6ec39b9bb12c73dee6d8b5844a742d
boguszmedykowski <bogusz.medykowski@gmail.com>
2023-03-06 19:47:26 +0100

    expand tests

--- workout/tests.py
+++ workout/tests.py
@@ -1,7 +1,9 @@
 from rest_framework.test import APITestCase
-from .models import Workout
+from django.test import TestCase
+from .models import Workout, Exercise
+from django.contrib.auth.models import User
 
-class Testcase(APITestCase):
+class TestWorkout(APITestCase):
 
     def setUp(self):
         Workout.objects.create(title="treningA")
@@ -16,33 +18,22 @@ class Testcase(APITestCase):
         self.assertEqual(response.status_code, 201)
         # self.assertEqual(response.content, b'{"url":"http://testserver/workout/workouts/2/","title":"treningE"}')
 
+class TestCreateWorkout(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        User.objects.create_user(username='test_user1', password='12345678')
+        Workout.objects.create(title='traning')
+        workout = Workout.objects.get(pk=1)
+        Exercise.objects.create(title='tescik', workout=workout)
 
+    def test_workout_content(self):
+        workout = Workout.objects.get(pk=1)
+        title = f"{workout.title}"
+        self.assertEqual(title, 'traning')
+        self.assertEqual(str(workout), 'traning')
 
-
-
-
-
-
-
-
-# class TestViews(APITestCase):
-#     url = "/workouts/workouts/1/"
-
-#     def setUp(self):
-#         Workout.objects.create(title="treningA")
-
-#     def test_get_workout(self):
-#         response = self.client.get(self.url)
-#         result = response.json()
-
-#         self.assertEqual(response.status_code, 200)
-#         self.assertEqual(result["title"], "treningA")
-
-#     def test_post_workout(self):
-
-#         data = {
-#             "title": "treningB"
-#         }
-#         factory = APIRequestFactory()
-#         request = factory.post(self.url, data, format='json')
-
+    def test_exercise_content(self):
+        exercise = Exercise.objects.get(pk=1)
+        title = f"{exercise.title}"
+        self.assertEqual(title, 'tescik')
+        self.assertEqual(str(exercise), 'tescik')
